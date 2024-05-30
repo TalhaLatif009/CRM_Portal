956ee2ffb73b99da52bdfae91ed28e1170ddc4d2
TalhaLatif009 <bhattitalha40@gmail.com>
2024-05-30 00:27:19 -0700

    Initial Code

---
+++ crm_site/mydb.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='root123'
+)
+
+cursorObject = dataBase.cursor()
+
+# Note: Hyphens are not allowed in database names, so use underscore instead.
+cursorObject.execute("CREATE DATABASE crm_portal")
+
+print("Database 'crm_portal' created successfully!")
+
+# Close the cursor and connection
+cursorObject.close()
+dataBase.close()
