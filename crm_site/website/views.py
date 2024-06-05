6c02ae511618820e86e2155bd1bd19efcbff3c9a
TalhaLatif009 <bhattitalha40@gmail.com>
2024-06-05 11:04:43 -0700

    rendering records

--- crm_site/website/views.py
+++ crm_site/website/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
+    
+	# Check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -16,13 +21,29 @@ def home(request):
             messages.success(request, 'There Was An Error While Logging In. Please Try Again...')
             return redirect('home')
             
+    return render(request, 'home.html', {'records':records})
 
-    return render(request, 'home.html', {})
 
 def logout_user(request):
     logout(request)
     messages.success(request, 'You Have Been Logged Out....')
     return redirect('home')
 
+
 def register_user(request):
-    return render(request, 'register.html', {})
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'register.html', {'form':form})
+
+	return render(request, 'register.html', {'form':form})
